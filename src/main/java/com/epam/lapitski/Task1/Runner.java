2eb6e8e98fc513b1e558b7703fbbfc1a9cf448d7
Dimitrii <dafon666@mail.ru>
2020-10-19 06:03:14 +0300

    Add Task1 and Task2

---
+++ src/main/java/com/epam/lapitski/Task1/Runner.java
@@ -0,0 +1,54 @@
+package com.epam.lapitski.Task1;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<Integer> randomElementsList = createRandomIntList(0, 20, 100);
+
+        System.out.println("Random List");
+        System.out.println(randomElementsList);
+        System.out.println("Without Stream API version 1");
+        System.out.println(countRepeatingElementsVersion1(randomElementsList));
+        System.out.println("Without Stream API version 2");
+        System.out.println(countRepeatingElementsVersion2(randomElementsList));
+        System.out.println("With Stream API");
+        System.out.println(countRepeatingElementsWithStream(randomElementsList));
+    }
+
+    private static <T> Map<T, Long> countRepeatingElementsWithStream(List<T> list) {
+        return list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private static <T> Map<T, Long> countRepeatingElementsVersion1(List<T> list) {
+        Map<T, Long> result = new HashMap<>();
+
+        for (T element : list) {
+            if (result.containsKey(element)) {
+                result.put(element, result.get(element) + 1);
+            } else {
+                result.put(element, (long) 1);
+            }
+        }
+
+        return result;
+    }
+
+    private static <T> Map<T, Long> countRepeatingElementsVersion2(List<T> list) {
+        Map<T, Long> result = new HashMap<>();
+
+        Set<T> uniqueElements = new HashSet<>(list);
+
+        for (T element : uniqueElements) {
+            result.put(element, (long) Collections.frequency(list, element));
+        }
+
+        return result;
+    }
+
+    private static List<Integer> createRandomIntList(int min, int max, int listSize) {
+        return new Random().ints(listSize, min, max).boxed().collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/epam/lapitski/Task2/Runner.java
@@ -0,0 +1,56 @@
+package com.epam.lapitski.Task2;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Runner {
+    public static final String EMPTY_VALUE_STRING = "";
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>(Arrays.asList(
+                "dfgh",
+                "dfklkll",
+                "dfghjhj",
+                "dftyuww"));
+        System.out.print("The longest prefix: ");
+        System.out.println(findLongestPrefix(stringList));
+        System.out.print("The longest prefix with stream: ");
+        System.out.println(findLongestPrefixWithStream(stringList));
+    }
+
+    private static String findLongestPrefix(List<String> list) {
+        if (isNullOrEmpty(list)) return EMPTY_VALUE_STRING;
+
+        while(list.remove(null)) {}
+
+        Collections.sort(list);
+
+        int listSize = list.size();
+        String first = list.get(0);
+        String last = list.get(listSize - 1);
+
+        int minLengthStringInList = Math.min(first.length(), last.length());
+
+        int counter = 0;
+        while (counter < minLengthStringInList &&
+                first.charAt(counter) == last.charAt(counter)) {
+            counter++;
+        }
+
+        return first.substring(0, counter);
+    }
+
+    private static String findLongestPrefixWithStream(List<String> list) {
+        if (isNullOrEmpty(list)) return EMPTY_VALUE_STRING;
+
+        String first = list.stream().filter(Objects::nonNull).min(String::compareTo).get();
+        String last = list.stream().filter(Objects::nonNull).max(String::compareTo).get();
+
+        int counter = (int) IntStream.range(0, first.length()).takeWhile(i -> first.charAt(i) == last.charAt(i)).count();
+
+        return first.substring(0, counter);
+    }
+
+    private static boolean isNullOrEmpty(List<String> list) {
+        return list == null || list.isEmpty();
+    }
+}

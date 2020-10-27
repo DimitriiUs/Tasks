d7692d8712f14396e3f603ba3d349b1ebb4fd42a
Dimitrii <dafon666@mail.ru>
2020-10-27 05:55:43 +0300

    Moved script.sh from Task3 to Epam2020

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../../../../../../../..
+cd ..
 if [ -e project_copy ] && [ -f log.txt ]
 then
   rm -r log.txt project_copy
@@ -9,7 +9,7 @@ mkdir "project_copy"
 cp -a  Epam2020/* project_copy
 cd project_copy
 mvn clean package --log-file log.txt
-mv log.txt ..
+mv log.txt .
 mv target/runner.jar .
 java -jar runner.jar
 read -rn1
